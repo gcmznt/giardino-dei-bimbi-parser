0f0760bc2568fc4fe9ee2d800a470ddcd0f6a084
Giacomo Zinetti <giacomo.zinetti@giko.it>
2018-04-13 11:43:56 +0200

    test express

---
+++ heroku.js
@@ -0,0 +1,39 @@
+process.env.DEBUG = "actions-on-google:*";
+const App = require("actions-on-google").DialogflowApp;
+const functions = require("firebase-functions");
+const { getData, YESTERDAY } = require("./getData");
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  getData(YESTERDAY).then(data => res.send(JSON.stringify(data)));
+});
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Example app listening on port 3000!")
+);
+
+// // a. the action name from the make_name Dialogflow intent
+// const NAME_ACTION = "activity";
+// // b. the parameters that are parsed from the make_name intent
+// const DATE_ARGUMENT = "date";
+
+// exports.giardinoDeiBimbi = functions.https.onRequest((request, response) => {
+//   const app = new App({ request, response });
+//   console.log("Request headers: " + JSON.stringify(request.headers));
+//   console.log("Request body: " + JSON.stringify(request.body));
+
+//   // c. The function that generates the silly name
+//   function getInfo(app) {
+//     let date = app.getArgument(DATE_ARGUMENT);
+//     getData()
+//       .then(data => app.tell(`${date} Caterina ha mangiato ${data[0].text}`))
+//       .catch();
+//   }
+//   // d. build an action map, which maps intent names to functions
+//   let actionMap = new Map();
+//   actionMap.set(NAME_ACTION, getInfo);
+
+//   app.handleRequest(actionMap);
+// });
--- index.js
+++ index.js
@@ -1,28 +1,35 @@
+"use strict";
+
 process.env.DEBUG = "actions-on-google:*";
 const App = require("actions-on-google").DialogflowApp;
 const functions = require("firebase-functions");
-const { getData, YESTERDAY } = require("./getData");
 
 // a. the action name from the make_name Dialogflow intent
-const NAME_ACTION = "activity";
+const NAME_ACTION = "make_name";
 // b. the parameters that are parsed from the make_name intent
-const DATE_ARGUMENT = "date";
+const COLOR_ARGUMENT = "color";
+const NUMBER_ARGUMENT = "number";
 
-exports.giardinoDeiBimbi = functions.https.onRequest((request, response) => {
+exports.sillyNameMaker = functions.https.onRequest((request, response) => {
   const app = new App({ request, response });
   console.log("Request headers: " + JSON.stringify(request.headers));
   console.log("Request body: " + JSON.stringify(request.body));
 
   // c. The function that generates the silly name
-  function getInfo(app) {
-    let date = app.getArgument(DATE_ARGUMENT);
-    getData()
-      .then(data => app.tell(`${date} Caterina ha mangiato ${data[0].text}`))
-      .catch();
+  function makeName(app) {
+    let number = app.getArgument(NUMBER_ARGUMENT);
+    let color = app.getArgument(COLOR_ARGUMENT);
+    app.tell(
+      "Alright, your silly name is " +
+        color +
+        " " +
+        number +
+        "! I hope you like it. See you next time."
+    );
   }
   // d. build an action map, which maps intent names to functions
   let actionMap = new Map();
-  actionMap.set(NAME_ACTION, getInfo);
+  actionMap.set(NAME_ACTION, makeName);
 
   app.handleRequest(actionMap);
 });
